c859d919147b5655667a98105c988253235403a7
Nikema Prophet <nikema@gmail.com>
2019-07-10 14:16:56 -0700

    Working on Menu.js

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -112,54 +112,57 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
-const articles = document.querySelector('.articles')
+const articles = document.querySelector(".articles");
 
 data.forEach(article => {
-  document.querySelector('.articles').appendChild(
+  articles.appendChild(
     createArticle(
       article.title,
       article.date,
       article.firstParagraph,
       article.secondParagraph,
       article.thirdParagraph
-    ))
-})
+    )
+  );
+});
 
-function createArticle(title, date, p1, p2, p3){
-
+function createArticle(title, date, p1, p2, p3) {
   // define new elements
-  const articleContainer = document.createElement('div')
-  const articleTitle = document.createElement('h2')
-  const articleDate = document.createElement('p')
-  const firstParagraph = document.createElement('p')
-  const secondParagraph = document.createElement('p')
-  const thirdParagraph = document.createElement('p')
-  const expandButton = document.createElement('span')
+  const articleContainer = document.createElement("div");
+  const articleTitle = document.createElement("h2");
+  const articleDate = document.createElement("p");
+  const firstParagraph = document.createElement("p");
+  const secondParagraph = document.createElement("p");
+  const thirdParagraph = document.createElement("p");
+  const expandButton = document.createElement("span");
 
   // append children
-  articleContainer.appendChild(articleTitle)
-  articleContainer.appendChild(articleDate)
-  articleContainer.appendChild(firstParagraph)
-  articleContainer.appendChild(secondParagraph)
-  articleContainer.appendChild(thirdParagraph)
-  articleContainer.appendChild(expandButton)
-  
-  
+  articleContainer.appendChild(articleTitle);
+  articleContainer.appendChild(articleDate);
+  articleContainer.appendChild(firstParagraph);
+  articleContainer.appendChild(secondParagraph);
+  articleContainer.appendChild(thirdParagraph);
+  articleContainer.appendChild(expandButton);
+
   // set class names
-  articleContainer.classList.add('article')
-  articleDate.classList.add('date')
-  expandButton.classList.add('expandButton')
+  articleContainer.classList.add("article");
+  articleDate.classList.add("date");
+  expandButton.classList.add("expandButton");
 
   // set text content
-  articleTitle.textContent = title
-  articleDate.textContent = date
-  firstParagraph.textContent = p1
-  secondParagraph.textContent = p2
-  thirdParagraph.textContent = p3
-  expandButton.textContent = 'expand'
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  firstParagraph.textContent = p1;
+  secondParagraph.textContent = p2;
+  thirdParagraph.textContent = p3;
+  expandButton.textContent = "expand";
 
   // make the article expand on click
-  expandButton.addEventListener('click', ()=> {articleContainer.classList.toggle('article-open')})
-return articleContainer
+  expandButton.addEventListener("click", () => {
+    articleContainer.classList.toggle("article-open");
+    articleContainer.classList.contains("article-open")
+      ? (expandButton.textContent = "collapse")
+      : (expandButton.textContent = "expand");
+  });
+  return articleContainer;
 }
-
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,27 +9,37 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
 
-  Step 1: Write a function that will create a menu component as seen below:
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+  // Step 1: Write a function that will create a menu component as seen below:
 
-  Pass the function an array as it's only argument.
+  // <div class="menu">
+  //   <ul>
+  //     {each menu item as a list item}
+  //   </ul>
+  // </div>
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
+  function createMenu(menuItems) {
+    const menu = document.createElement('div')
+    const ul = document.createElement('ul')
 
-  Step 3: Using a DOM selector, select the menu button currently on the DOM.
+    menu.appendChild(ul)
+
+  // Pass the function an array as it's only argument.
+
+  // Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+    
+    menuItems.map(item => {
+      const li = document.createElement('li')
+    })
+  // Add those items to the <ul>
+
+  // Step 3: Using a DOM selector, select the menu button currently on the DOM.
   
-  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+  // Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
-  Step 5: return the menu component.
+  // Step 5: return the menu component.
 
-  Step 6: add the menu component to the DOM.
-  
-*/
+  // Step 6: add the menu component to the DOM.
+  }
+
